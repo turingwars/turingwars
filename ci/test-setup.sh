899997d792a930862c7af8e02ef7e964ff674617
Hadrien Milano <hadrien.milano@gmail.com>
2018-08-24 23:43:46 +0200

    Strengthen ci test script

--- ci/test-setup.sh
+++ ci/test-setup.sh
@@ -1,9 +1,34 @@
 #!/bin/bash -xe
 
-turingwars &
+logfile=./test.log
+
+cleanup() {
+    kill $pid
+    rm $logfile
+}
+is_dead() {
+    ! kill -0 $pid
+}
+trap cleanup EXIT
+
+
+# main():
+
+turingwars | tee $logfile &
 pid=$!
 
+# $pid is now the pid of the launcher, which is different from that of the server.
+# We now attempt to find the pid of the server.
 sleep 1
-curl http://localhost:3000/ > /dev/null
+until is_dead || grep -q "TuringWars started with pid" "$logfile"; do
+    sleep 1
+done
+pid=$(grep "TuringWars started with pid" "$logfile" | sed 's/TuringWars started with pid \([0-9][0-9]*\)./\1/')
+echo "Got pid $pid"
 
-kill $pid
+# Wait until server is either running or dead
+until is_dead || grep -q "Server is listening" "$logfile"; do
+    sleep 1
+done
+
+curl http://localhost:3000/ > /dev/null
--- web/bin/turingwars.js
+++ web/bin/turingwars.js
@@ -2,13 +2,16 @@
 var child_process = require('child_process');
 var path = require('path');
 
-child_process.fork(path.join(__dirname, '../lib/server/boot.js'), {
+const child = child_process.fork(path.join(__dirname, '../lib/server/boot.js'), {
     execArgv: ['-r', require.resolve('source-map-support/register')],
     env: {
         'NODE_PATH': appendPath(process.env.NODE_PATH, path.join(__dirname, '../lib')),
         'NODE_ENV': 'production'
     }
 });
+
+// Note: This is used by automated test scripts. Be cautions if you change this string.
+console.log(`TuringWars started with pid ${child.pid}.`);
 
 function appendPath(original, ext) {
     return original ? original + ':' + ext : ext;
